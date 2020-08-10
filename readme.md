dd534d83d432d474d87b19b74c8c2cbec1cf2ae7
Prisca Roseline <prisca.ros@onja.org>
2020-08-10 08:00:49 +0300

    Expained my code in readme file

--- readme.md
+++ readme.md
@@ -1,92 +1,9 @@
-# WKP6 - Hungry Student Restaurant
+##  WKP6-rEPORT
 
-### Another website about food, really...
+1. I shared the project into many parts and I tried to solve it step by step.First, I checked the input input box whether it checked or not. If it is checked, I filtered it and then maped.
 
-Yes, but who doesn't like food? 😄
+2.If had more times, I would constractor my code in order to get it more understandable and neater. And also, I would add more comments.
 
-This time, we're going to build a food delivery app, where we can see the meal options, select them, and see the result in our cart.
+3. The most changelling part for me was to add the menu into the order list.
 
-### Users must be able to :
-
--   See a list of five meal options
--   See the prices of each meal option
--   Add a meal to the cart
--   Filter the list to see only the vegetarian meals, or only the spicy ones
--   See their total bill
--   “Check out” - this doesn’t have to process payment, just simply display
-    a message stating the order as been confirmed.
-
-### Bonus :
-
--   Change the quantity of meals in the cart
--   Remove a meal from the cart
-
-Here is an example of layout :
-
-![assets/1.png](assets/1.png)
-
-And when I click on the _Confirm Order_ button, I can display a message to show the total bill from the order.
-
-![assets/2.png](assets/2.png)
-
-### Food list
-
-Here's the food object :
-
-```jsx
-const foods = [
-	{
-		id: 'ravitoto',
-		price: 5000,
-		title: 'Ravitoto',
-		spicy: true,
-		vegetarian: false,
-	},
-	{
-		id: 'pasta',
-		price: 4000,
-		title: 'Pasta',
-		spicy: true,
-		vegetarian: true,
-	},
-	{
-		id: 'burger',
-		price: 5000,
-		title: 'Burger',
-		spicy: false,
-		vegetarian: false,
-	},
-	{
-		id: 'rice',
-		price: 2000,
-		title: 'Rice and Leaves',
-		spicy: false,
-		vegetarian: true,
-	},
-	{
-		id: 'mofogasy',
-		price: 500,
-		title: 'Mofogasy',
-		spicy: false,
-		vegetarian: false,
-	},
-];
-```
-
-The **id** here is a unique string, is role is to identify an element inside the list. The title will be the food name shown to the user.
-
-You'll also have an **order** collection, where you will push a food object every time the user add an element to his order.
-
-### Maps, Filter, Reduce, and other friends
-
--   To generate any list of element (for example, the list of food), **Map** is really useful.
--   To get the full price of list of items, **Reduce** is the perfect use case.
--   To count how many times an item is in a list, you could **Filter** the list and see the length of the result, or use a **Reduce** to count all of the instances.
-
-But this is just one way to think about that. Feel free to try any other logic you have in mind.
-
-### 💡Tips
-
--   Don't forget to clean your code, and update your readme before Monday morning.
-
-Good luck!
+4. I would like more explanation about reduce method if it is possible please.
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const list = document.querySelector('.list');
 const cardOrder = document.querySelector('#cards-order');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
+const inputEl = document.querySelector('input');
 
 
 // cards array object
@@ -53,7 +54,7 @@ const foods = [
 
 const generateCards = () => {
   const html = foods.map(food => `
-      <li class="items">${food.title}
+      <li class="items" id="${food.id}">${food.title}
         <span>${food.price}</span>
         <button type="add" class="add-btn">Add</button>
       </li>`
@@ -67,7 +68,7 @@ spicyFood.addEventListener('change', function () {
     const foodList = foods.filter(food => food.spicy)
     const myFood = foodList.map(food =>
       `
-                  <li class="items">${food.title}
+                  <li class="items" id="${food.id}">${food.title}
                     <span>${food.price}</span>
                     <button type="add" class="add-btn">Add</button>
                   </li>
@@ -82,7 +83,7 @@ vegetarianFood.addEventListener('change', function () {
   if (this.checked) {
     const filteredVegetarianFood = foods.filter(food => food.vegetarian)
     const mapedVeg = filteredVegetarianFood.map(food => `
-                  <li class="items">${food.title}
+                  <li class="items" id="${food.id}">${food.title}
                     <span>${food.price}</span>
                     <button type="add" class="add-btn">Add</button>
                   </li>
@@ -92,20 +93,44 @@ vegetarianFood.addEventListener('change', function () {
   };
 });
 
+inputEl.addEventListener('change', function () {
+  if (spicyFood.checked && vegetarianFood.checked) {
+    const filteredVegetarianFood = foods.filter(food => food.vegetarian && food.spicy);
+    const mapedBothVegAndSpicy = filteredVegetarianFood.map(food => `
+                  <li class="items" id="${food.id}">${food.title}
+                    <span>${food.price}</span>
+                    <button type="add" class="add-btn">Add</button>
+                  </li>
+              `
+    ).join('');
+    list.innerHTML = mapedBothVegAndSpicy;
+  };
+});
+
 // Add the food list into order list
+
+listsOrder = [];
 const addOrder = (e) => {
   if (e.target.matches('button.add-btn')) {
-    console.log('hi');
-    // const id = parent
-    // const findId = foods.find(item => item.id === id)
-    const html = foods.map(food => `
-      <li class="items">${food.title}
-        <span>x2</span>
-        <span>${food.price}</span>
-      </li>`
-      ).join('');
-    cardOrder.innerHTML = html;
-  };
+    const order = event.target.closest('.items');
+    const items = {
+      id: order.querySelector('.items').textContent,
+      title: order.querySelector('.items').textContent,
+      price: order.querySelector('span').textContent
+    };
+
+    listsOrder.push(order);
+    const myOrderHtml = `
+      <li class="items" id="${items.id}">
+              <div>${items.title}</div>
+              <span>&times;</span>
+              <span class="price">${items.price}</span>
+          </div>
+      </li>
+  `;
+    cardOrder.insertAdjacentHTML('beforeend', myOrderHtml)
+  }
+
 };
 
 // Submit the bill
